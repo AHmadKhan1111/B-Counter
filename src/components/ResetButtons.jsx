bdc170ade3a91af23ac7f7a3746d88014bc8e049
Ahmad Raza Khan <133381876+AHmadKhan1111@users.noreply.github.com>
2025-10-06 19:37:46 +0500

    Update and rename Reset.jsx to ResetButtons.jsx

--- src/components/ResetButtons.jsx
+++ src/components/ResetButtons.jsx
@@ -1,10 +1,11 @@
 import { ResetIcon } from "@radix-ui/react-icons";
-export default function Reset({ setCount, count }) {
+export default function ResetButtons({ setCount, count }) {
   return (
     <>
-      <button className="reset-btn" >
+      <button className="reset-btn" onClick={() => setCount(0)}>
         <ResetIcon className="reset-btn-icon" />
       </button>
     </>
   );
 }
+
